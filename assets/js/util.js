5e374dd566389910232c70e3c907c77354e9c534
devvy <70729129+devvyyy@users.noreply.github.com>
2024-12-03 21:38:36 -0500

    project 5
    clean up code

--- assets/js/util.js
+++ assets/js/util.js
@@ -34,6 +34,24 @@
 
 	};
 
+	$('a[href^="#"]').click(function () {
+		$('html, body').animate({
+			scrollTop: $('[name="' + $.attr(this, 'href').substr(1) + '"]').offset().top
+		}, 500);
+	
+		return false;
+	});
+
+	document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+		anchor.addEventListener('click', function (e) {
+			e.preventDefault();
+	
+			document.querySelector(this.getAttribute('href')).scrollIntoView({
+				behavior: 'smooth'
+			});
+		});
+	});
+
 	/**
 	 * Panel-ify an element.
 	 * @param {object} userConfig User config.
@@ -134,6 +152,8 @@
 						}, config.delay);
 
 				};
+
+
 
 			// Vendor fixes.
 				$this
